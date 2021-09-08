8daa3bd02e099afbcdc5e751c2b16c1f76209b87
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2021-09-08 03:35:07 +0200

    Implementing the init(String, Long) method in the TestInfoScreenController class.

--- src/main/java/com/ramsey/rexam/view/TestInfoScreenController.java
+++ src/main/java/com/ramsey/rexam/view/TestInfoScreenController.java
@@ -1,9 +1,10 @@
 package com.ramsey.rexam.view;
 
+import com.ramsey.rexam.beans.ExamBean;
 import com.ramsey.rexam.entity.Exam;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class TestInfoScreenController {
 	
@@ -11,16 +12,24 @@ public class TestInfoScreenController {
 	public TextField passingScoreTextField;
 	@FXML
 	public TextField examTimeTextField;
+	@FXML
+	public Text examNameText;
 	private String studentName;
 	private Exam exam;
+	private ExamBean examBean;
 	
 	public void init(String studentName, Long examId) {
 		
-		//
+		this.studentName = studentName;
+		examBean = ExamBean.getInstance();
+		exam = examBean.getExam(examId);
+		examNameText.setText(String.format("%s Test", exam.getName()));
+		passingScoreTextField.setText(String.format("%.0f%%", exam.getPassingScore()));
+		examTimeTextField.setText(String.format("%d minutes", exam.getTimeInMinutes()));
 		
 	}
 	
-	public void continueButtonClicked(MouseEvent mouseEvent) {
+	public void continueButtonClicked() {
 		
 		//
 		
