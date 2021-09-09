cbfabc0d5ca560086a6168f86a5e6e07fd636b57
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2021-09-09 02:09:05 +0200

    Adding the check, and close icons. Adding a Question property to the QuestionPanelController class. Using a combo of ScrollPane, VBox, and HBox instead of the GridPanel. Shuffling the questions, and there answers before displaying them. Implementing the startTimer(), refreshButtons(), startTimer(), canFinish(), refreshTotalQuestions(), initializeTotalQuestions(), handleSelectAnswerEvent(Pair<Question, Answer) methods to the TestScreenController class.

--- src/main/java/com/ramsey/rexam/view/QuestionPanelController.java
+++ src/main/java/com/ramsey/rexam/view/QuestionPanelController.java
@@ -1,5 +1,6 @@
 package com.ramsey.rexam.view;
 
+import com.ramsey.rexam.entity.Question;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -10,5 +11,6 @@ public class QuestionPanelController {
 	public Text questionText;
 	@FXML
 	public VBox answersVBox;
+	public Question question;
 	
 }
